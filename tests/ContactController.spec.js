241bb7570d5ab3d84e0c282c666bb7d971803572
Christian Whitesides <whitesidescr@gmail.com>
2016-12-12 17:00:37 -0700

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -1,7 +1,7 @@
 describe('ContactController', function () {
-	var $controller;
-
 	beforeEach(module('app'));
+
+	var $controller;
 
 	beforeEach(inject(function (_$controller_) {
 		$controller = _$controller_;
@@ -12,20 +12,20 @@ describe('ContactController', function () {
 		var $scope = {};
 		$controller('ContactController', {$scope: $scope});
 
-		expect($scope.name).toBe('Steve Jobs');
+		expect($scope.name).toBe('Bill Gates');
 	});
 
 	it('should have a title set to "Founder"', function () {
 		var $scope = {};
 		$controller('ContactController', {$scope: $scope});
 
-		expect($scope.title).toBe('Cleaner');
+		expect($scope.title).toBe('Founder');
 	});
 
 	it('should have an email set to "bill@microsoft.com"', function () {
 		var $scope = {};
 		$controller('ContactController', {$scope: $scope});
 
-		expect($scope.email).toBe('tim@apple.com');
+		expect($scope.email).toBe('bill@microsoft.com');
 	});
 });
